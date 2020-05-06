94e699fe9211bc1079a63e569bad3d006f786dde
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-06 23:59:17 +0100

    add: create li

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body>
 	<header>
 		<input type="text" placeholder="Enter an activity...">
-		<button>
+		<button id="add">
 			<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 				 viewBox="0 0 16 16" xml:space="preserve">
 			<g>
@@ -18,6 +18,12 @@
 			</svg>
 		</button>
 	</header>
+
+	<div class="container">
+		<ul class="todo">
+			<li>This is an item</li>
+		</ul>
+	</div>
 
 	<script src="./js/main.js"></script>
 </body>
---
+++ js/main.js
@@ -0,0 +1,6 @@
+// window.onload = function () {
+// 	document.getElementById('add');
+// }
+document.getElementById('add').addEventListener('click', function() {
+	var value = document
+});

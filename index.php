f1a0590c21c04d7e2a93bd06ed11de4e181e1f23
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-07 22:28:47 +0100

    feat: add js to add list item

--- index.php
+++ index.php
@@ -12,21 +12,15 @@
 </head>
 <body>
 	<header>
-		<input type="text" placeholder="Enter an activity...">
+		<input type="text" placeholder="Enter an activity..." id="item">
 		<button id="add">
-			<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-				 viewBox="0 0 16 16" xml:space="preserve">
-			<g>
-				<path class="fill" d="M16,8c0,0.5-0.5,1-1,1H9v6c0,0.5-0.5,1-1,1s-1-0.5-1-1V9H1C0.5,9,0,8.5,0,8s0.5-1,1-1h6V1c0-0.5,0.5-1,1-1
-					s1,0.5,1,1v6h6C15.5,7,16,7.5,16,8z"/>
-			</g>
-			</svg>
+			<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 16 16" xml:space="preserve"><g><path class="fill" d="M16,8c0,0.5-0.5,1-1,1H9v6c0,0.5-0.5,1-1,1s-1-0.5-1-1V9H1C0.5,9,0,8.5,0,8s0.5-1,1-1h6V1c0-0.5,0.5-1,1-1s1,0.5,1,1v6h6C15.5,7,16,7.5,16,8z"/></g></svg>
 		</button>
 	</header>
 
 	<div class="container">
 		<ul class="todo" id="todo">
-			<li>
+<!-- 			<li>
 				This is an item
 				<div class="buttons">
 					<button class="remove">
@@ -43,7 +37,7 @@
 						</svg>
 					</button>
 				</div>
-			</li>
+			</li> -->
 		</ul>
 		<ul class="todo" id="completed">
 			
